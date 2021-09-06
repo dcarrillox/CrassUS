159fb1de5c56324415bb155973893abe623f4f29
dcarrillox <dcarrillo.bioinf@gmail.com>
2021-09-06 13:02:17 +0200

    switch to hmmer wrapper and 5k length filter

--- scripts/filter_rehead_assemblies.py
+++ scripts/filter_rehead_assemblies.py
@@ -15,7 +15,7 @@ to_write_table = list()
 n = 1
 for id, record in sorted_ids_records.items():
     to_add_table = [record.id]
-    if len(record.seq) >= 20000:
+    if len(record.seq) >= 5000:
         rehead = f"{snakemake.wildcards.sample}_{n}_{len(record.seq)}"
         to_add_table.append(rehead)
         record.id = rehead
